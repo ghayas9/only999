3288f5cf165f9aba2a182a6947a4904eacfdf7dc
ghayas9 <ghayasudin999@gmail.com>
2021-11-10 21:57:19 +0500

    3

--- src/TopBar.js
+++ src/TopBar.js
@@ -15,6 +15,24 @@ function TopBar(){
                 <i className="fab fa-whatsapp"></i>
                 </div>
             </div>
+
+            <div className="row logobar">
+            <i className="fas fa-bars col-4 menu"></i>
+            <div className='col-md-8 col-4 logo'>
+            <img src="https://cdn.shopify.com/s/files/1/0607/8618/0350/files/Logo_155x.png?v=1635777207" alt=""  />
+            </div>
+            <span className='col-4 uss'>
+            <i className="fas fa-search"></i>
+            <i className="fab fa-shopify"></i>
+            <i className="fas fa-user hdn"></i>
+            <i className="fas fa-sliders-h hdn"></i>
+            </span>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li>Apparel</li>
+                </ul>
+            </div>
         </div>
     )
 }
